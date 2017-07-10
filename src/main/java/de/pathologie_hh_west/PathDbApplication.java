c53e4cbc3efcb44bcd10915382b28948ab582e66
e96wic <wichern.eike@googlemail.com>
2017-07-10 21:07:31 +0200

    Changed database init to switch to h2 whenever mysql is not available

--- src/main/java/de/pathologie_hh_west/PathDbApplication.java
+++ src/main/java/de/pathologie_hh_west/PathDbApplication.java
@@ -1,14 +1,23 @@
 package de.pathologie_hh_west;
 
+import com.mysql.jdbc.Driver;
+import com.mysql.jdbc.exceptions.jdbc4.CommunicationsException;
 import de.pathologie_hh_west.data.FallRepository;
 import de.pathologie_hh_west.data.PatientRepository;
 import de.pathologie_hh_west.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -20,6 +29,49 @@ public class PathDbApplication {
 		SpringApplication.run(PathDbApplication.class, args);
 	}
 }
+
+@Component
+class DBSetup {
+	@Value("${mysql.datasource.url}")
+	private String url;
+	@Value("${mysql.datasource.username}")
+	private String username;
+	@Value("${mysql.datasource.password}")
+	private String password;
+	
+	@Bean
+	@Primary
+//	@ConfigurationProperties(value = "mysql.datasource")
+	public DataSource mysqlDataSource(DataSourceProperties properties) {
+		try {
+			DataSource dataSource = DataSourceBuilder
+					.create()
+					.driverClassName(Driver.class.getName())
+					.url(url)
+					.username(username)
+					.password(password)
+					.build();
+
+			dataSource.getConnection();
+			return dataSource;
+		} catch (CommunicationsException e) {
+			return DataSourceBuilder
+					.create()
+					.url("jdbc:h2:mem:AZ;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
+					.driverClassName("org.h2.Driver")
+					.username("sa")
+					.build();
+		} catch (SQLException e) {
+			return DataSourceBuilder
+					.create()
+					.url("jdbc:h2:mem:AZ;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
+					.driverClassName("org.h2.Driver")
+					.username("sa")
+					.build();
+		}
+	}
+}
+
 @Component
 class Test implements CommandLineRunner{
 	@Autowired
